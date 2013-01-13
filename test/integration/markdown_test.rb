1594d6fce7bd91e4fb4f410a1216316f625456ba
Nico Hagenburger <nico@hagenburger.net>
2013-01-13 18:10:57 +0100

    refactored tests

--- test/integration/markdown_test.rb
+++ test/integration/markdown_test.rb
@@ -5,19 +5,18 @@ class MarkdownTest < Test::Unit::TestCase
 
   def render(file)
     template = Tilt.new(file)
-    html     = template.render
-    format_html(html)
+    template.render
   end
 
-  def format_html(html)
-    html.strip!
-    html.gsub! /\n+/, "\n      "
-    %Q(      #{html}\n)
+  def assert_markdown(expected, file)
+    expected = expected.gsub(/\s+/m, ' ').gsub('$', '\\$').strip
+    given    = render(File.join(%W(test fixtures markdown #{file})))
+    given    = given.gsub(/\s+/m, ' ').strip
+    assert_match /#{expected}/, given
   end
 
   def test_examples
-    html = render('test/fixtures/markdown/example.md')
-    assert_equal <<-HTML, html
+    assert_markdown <<-HTML, 'example.md'
       <div class="livingstyleguide--example">
         <button class="button">Test</button>
       </div>
@@ -25,27 +24,25 @@ class MarkdownTest < Test::Unit::TestCase
   end
 
   def test_text
-    html = render('test/fixtures/markdown/text.md')
-    assert_equal <<-HTML, html
+    assert_markdown <<-HTML, 'text.md'
       <h2 class="livingstyleguide--headline">Hello World</h2>
       <p class="livingstyleguide--paragraph">Lorem ipsum <strong>dolor</strong> sit amet, <code class="livingstyleguide--code">&lt;consectetur&gt; adipiscing</code> elit. Sed a pulvinar turpis.</p>
       <ul class="livingstyleguide--unordered-list">
-      <li class="livingstyleguide--unordered-list-item">Lorem</li>
-      <li class="livingstyleguide--unordered-list-item">Ipsum</li>
-      <li class="livingstyleguide--unordered-list-item">Dolor</li>
+        <li class="livingstyleguide--unordered-list-item">Lorem</li>
+        <li class="livingstyleguide--unordered-list-item">Ipsum</li>
+        <li class="livingstyleguide--unordered-list-item">Dolor</li>
       </ul>
       <h3 class="livingstyleguide--sub-headline">More Lorem</h3>
       <ol class="livingstyleguide--ordered-list">
-      <li class="livingstyleguide--ordered-list-item">Lorem</li>
-      <li class="livingstyleguide--ordered-list-item">Ipsum</li>
-      <li class="livingstyleguide--ordered-list-item">Dolor</li>
+        <li class="livingstyleguide--ordered-list-item">Lorem</li>
+        <li class="livingstyleguide--ordered-list-item">Ipsum</li>
+        <li class="livingstyleguide--ordered-list-item">Dolor</li>
       </ol>
     HTML
   end
 
   def test_code
-    html = render('test/fixtures/markdown/code.md')
-    assert_equal <<-HTML, html
+    assert_markdown <<-HTML, 'code.md'
       <pre class="livingstyleguide--code-block"><code class="livingstyleguide--code"><b><i>.my-class</i></b> {
         <b>color:</b> <b>red</b>;
       }</code></pre>
@@ -53,11 +50,10 @@ class MarkdownTest < Test::Unit::TestCase
   end
 
   def test_variables
-    html = render('test/fixtures/markdown/variables.md')
-    assert_equal <<-HTML, html
+    assert_markdown <<-HTML, 'variables.md'
       <ul class="livingstyleguide--color-swatches">
-      <li class="livingstyleguide--color-swatch $blue">$blue</li>
-      <li class="livingstyleguide--color-swatch $green">$green</li>
+        <li class="livingstyleguide--color-swatch $blue">$blue</li>
+        <li class="livingstyleguide--color-swatch $green">$green</li>
       </ul>
     HTML
   end
