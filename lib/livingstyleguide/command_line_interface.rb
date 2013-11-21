4ac9a91343379a8a1fc6939eb6a2369ee6a7ccb7
Nico Hagenburger <nico@hagenburger.net>
2013-11-21 11:01:33 +0100

    output livingstyleguide to compass’ output directory

--- lib/livingstyleguide/command_line_interface.rb
+++ lib/livingstyleguide/command_line_interface.rb
@@ -10,7 +10,10 @@ module LivingStyleGuide
     def compile(file)
       Compass.add_project_configuration
       html = Tilt.new(file).render
-      puts html
+      output_file = file.sub(/\.s[ac]ss\.lsg$/, '')
+      output_file.sub! /^#{Compass.configuration.sass_dir}/, Compass.configuration.css_dir
+      File.write output_file, html
+      puts "Successfully generated a living style guide at #{output_file}."
     end
 
   end
