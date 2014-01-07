509cdff198c50b2f0dbef71e63a21655da93dd6f
Nico Hagenburger <nico@hagenburger.net>
2014-01-08 00:30:52 +0100

    allow additional sass/scss code after yaml configuration

--- test/integration/standalone_test.rb
+++ test/integration/standalone_test.rb
@@ -4,10 +4,25 @@ require 'tilt'
 class MarkdownTest < Test::Unit::TestCase
 
   def test_standalone_project
-    html = Tilt.new('test/fixtures/standalone/styleguide.html.lsg').render
+    html = render('test/fixtures/standalone/styleguide.html.lsg')
     assert_match %r(background: red), html
     assert_match %r(<button class="button">), html
     assert_match %r(<title>My Nice &amp; Beautiful Living Style Guide</title>), html
+  end
+
+  def test_additional_scss_code
+    html = render('test/fixtures/standalone/styleguide-with-scss.html.lsg')
+    assert_match %r(#test { color: red; }), html
+  end
+
+  def test_additional_sass_code
+    html = render('test/fixtures/standalone/styleguide-with-sass.html.lsg')
+    assert_match %r(#test { color: green; }), html
+  end
+
+  private
+  def render(file)
+    Tilt.new(file).render.gsub(/\s+/, ' ')
   end
 
 end
