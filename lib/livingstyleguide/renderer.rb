cd587a9969d2ece225a7072b91d85bdd2a8f98f9
Nico Hagenburger <nico@hagenburger.net>
2014-01-07 23:39:43 +0100

    allow custom titles

--- lib/livingstyleguide/renderer.rb
+++ lib/livingstyleguide/renderer.rb
@@ -3,12 +3,18 @@ require 'erb'
 module LivingStyleGuide::Renderer
 
   def render_living_style_guide
-    @css     = self.render
-    template = File.read(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'layouts', 'default.html.erb'))
-    markdown = LivingStyleGuide.markdown || ""
-    @html    = LivingStyleGuide::RedcarpetTemplate.new{ markdown }.render
+    data = Class.new do
+      attr_accessor :css, :html, :title
+      include ERB::Util
+      def get_binding; binding end
+    end.new
+    data.css   = self.render
+    markdown   = LivingStyleGuide.markdown || ''
+    data.html  = LivingStyleGuide::RedcarpetTemplate.new{ markdown }.render
+    data.title = @options[:living_style_guide][:title]
     LivingStyleGuide.reset
-    ERB.new(template).result(binding)
+    template   = File.read(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'layouts', 'default.html.erb'))
+    ERB.new(template).result(data.get_binding)
   end
 
 end
