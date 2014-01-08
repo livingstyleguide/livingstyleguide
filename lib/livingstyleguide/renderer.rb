89e57a2b3c27aed85bb528fafd081db65c2348d7
Nico Hagenburger <nico@hagenburger.net>
2014-01-09 00:16:05 +0100

    linking of javascript files

--- lib/livingstyleguide/renderer.rb
+++ lib/livingstyleguide/renderer.rb
@@ -4,14 +4,20 @@ module LivingStyleGuide::Renderer
 
   def render_living_style_guide
     data = Class.new do
-      attr_accessor :css, :html, :title
+      attr_accessor :css, :html, :title, :header, :footer
       include ERB::Util
       def get_binding; binding end
     end.new
-    data.css   = self.render
-    markdown   = LivingStyleGuide.markdown || ''
-    data.html  = LivingStyleGuide::RedcarpetTemplate.new{ markdown }.render
-    data.title = @options[:living_style_guide][:title]
+    data.css    = self.render
+    markdown    = LivingStyleGuide.markdown || ''
+    data.html   = LivingStyleGuide::RedcarpetTemplate.new{ markdown }.render
+    data.title  = @options[:living_style_guide][:title]
+    data.header = (@options[:living_style_guide][:'javascript-before'] || []).map do |src|
+      %Q(<script src="#{src}"></script>)
+    end.join("\n")
+    data.footer = (@options[:living_style_guide][:'javascript-after'] || []).map do |src|
+      %Q(<script src="#{src}"></script>)
+    end.join("\n")
     LivingStyleGuide.reset
     template   = File.read(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'layouts', 'default.html.erb'))
     ERB.new(template).result(data.get_binding)
