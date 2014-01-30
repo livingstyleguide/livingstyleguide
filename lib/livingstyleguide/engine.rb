51b87ec453b1018dc3664524fba02c3d5fc6bad1
Nico Hagenburger <nico@hagenburger.net>
2014-01-30 23:48:15 +0100

    code refactoring

---
+++ lib/livingstyleguide/engine.rb
@@ -0,0 +1,82 @@
+module LivingStyleGuide
+
+  class Engine
+    attr_accessor :markdown, :options
+
+    def initialize(source, options, sass_options)
+      @source = source
+      @options = options
+      @sass_options = sass_options
+      @markdown = ''
+    end
+
+    def render
+      data = TemplateData.new(self)
+      template = File.read(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'layouts', 'default.html.erb'))
+      ERB.new(template).result(data.get_binding)
+    end
+
+    def css
+      sass_engine.render
+    end
+
+    def html
+      LivingStyleGuide::RedcarpetTemplate.new{ markdown }.render
+    end
+
+    private
+    def sass_engine
+      return @sass_engine if @sass_engine
+      sass_options = @sass_options.clone
+      sass_options[:living_style_guide] = self
+      @sass_engine = ::Sass::Engine.new(@source, sass_options)
+    end
+
+  end
+
+  class TemplateData
+    include ERB::Util
+
+    def initialize(engine)
+      @engine = engine
+    end
+
+    def get_binding
+      binding
+    end
+
+    def title
+      @engine.options[:title]
+    end
+
+    def css
+      @engine.css
+    end
+
+    def html
+      @engine.html
+    end
+
+    def head
+      (@engine.options[:javascript_before] || []).map do |src|
+        %Q(<script src="#{src}"></script>)
+      end.join("\n")
+    end
+
+    def header
+      contents = [@engine.options[:header]]
+      contents.join("\n")
+    end
+
+    def footer
+      contents = [@engine.options[:footer]]
+      contents << (@engine.options[:javascript_after] || []).map do |src|
+        %Q(<script src="#{src}"></script>)
+      end
+      contents.join("\n")
+    end
+
+  end
+
+end
+
