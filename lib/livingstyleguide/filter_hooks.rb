d85dce5797eb9b22294b71fff729ee9528ca3f06
Nico Hagenburger <nico@hagenburger.net>
2014-02-06 00:59:14 +0100

    run last hook

--- lib/livingstyleguide/filter_hooks.rb
+++ lib/livingstyleguide/filter_hooks.rb
@@ -1,21 +1,30 @@
 module LivingStyleGuide::FilterHooks
   def self.included(base)
     base.class_eval do
-      extend ClassMethods
-      include ClassMethods
+      extend Methods
+      include Methods
     end
   end
 
-  module ClassMethods
+  module Methods
     def run_filter_hook(name, source)
-      _hooks[name].each do |callback|
-        if callback.kind_of?(Symbol)
-          source = send(callback, source)
-        else
-          source = instance_exec(source, &callback)
-        end
+      _hooks[name].each do |hook|
+        source = run_single_hook(hook, source)
       end
       source
+    end
+
+    def run_last_filter_hook(name, source)
+      run_single_hook(_hooks[name].last, source)
+    end
+
+    private
+    def run_single_hook(hook, source)
+      if hook.kind_of?(Symbol)
+        send(hook, source)
+      else
+        instance_exec(source, &hook)
+      end
     end
   end
 end
