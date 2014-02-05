8310df28fb8a32380a237d4dcc6e1b55b9b43f15
Nico Hagenburger <nico@hagenburger.net>
2014-02-06 00:59:14 +0100

    run last hook

--- test/unit/filter_hooks_test.rb
+++ test/unit/filter_hooks_test.rb
@@ -12,20 +12,30 @@ describe LivingStyleGuide::FilterHooks do
     end
   end
 
-  it "should filter one filter" do
-    @class.filter { |text| text * 2 }
-    @class.run_filter_hook(:filter, "hello world").must_equal "hello worldhello world"
+  describe "running filters after each other" do
+    it "should filter one filter" do
+      @class.filter { |text| text * 2 }
+      @class.run_filter_hook(:filter, "hello world").must_equal "hello worldhello world"
+    end
+
+    it "should filter several filter in a row" do
+      @class.filter { |text| text * 2 }
+      @class.filter { |text| text.gsub(/worldhello/, 'beautiful') }
+      @class.run_filter_hook(:filter, "hello world").must_equal "hello beautiful world"
+    end
+
+    it "should work on instances" do
+      @class.filter { |text| text * 2 }
+      @class.new.run_filter_hook(:filter, "hello world").must_equal "hello worldhello world"
+    end
   end
 
-  it "should filter several filter in a row" do
-    @class.filter { |text| text * 2 }
-    @class.filter { |text| text.gsub(/worldhello/, 'beautiful') }
-    @class.run_filter_hook(:filter, "hello world").must_equal "hello beautiful world"
-  end
-
-  it "should work on instances" do
-    @class.filter { |text| text * 2 }
-    @class.new.run_filter_hook(:filter, "hello world").must_equal "hello worldhello world"
+  describe "run only the latest added filter" do
+    it "should should run only one filter" do
+      @class.filter { |text| text.gsub(/animal/, 'fish') }
+      @class.filter { |text| text.gsub(/animal/, 'bird') }
+      @class.run_last_filter_hook(:filter, "the animal").must_equal "the bird"
+    end
   end
 
 end
