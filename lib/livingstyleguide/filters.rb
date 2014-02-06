b01da28ce5d2ceb842f982bb06266239119516a8
Nico Hagenburger <nico@hagenburger.net>
2014-02-06 09:58:54 +0100

    added font example filter; allow to suppress code blocks in examples

--- lib/livingstyleguide/filters.rb
+++ lib/livingstyleguide/filters.rb
@@ -3,4 +3,5 @@ require 'livingstyleguide/filters/full_width'
 require 'livingstyleguide/filters/haml'
 require 'livingstyleguide/filters/javascript'
 require 'livingstyleguide/filters/add_wrapper_class'
+require 'livingstyleguide/filters/font_example'
 
