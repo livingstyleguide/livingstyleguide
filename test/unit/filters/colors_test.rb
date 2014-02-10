d98fc5bed9731889f2bea7c1d1e5a7218b96d9b8
Nico Hagenburger <nico@hagenburger.net>
2014-02-10 20:44:42 +0100

    implemented multi line colors

--- test/unit/filters/colors_test.rb
+++ test/unit/filters/colors_test.rb
@@ -9,7 +9,7 @@ class ColorsTest < ExampleTestCase
       assert_render_equals <<-INPUT, <<-OUTPUT
         @colors variables/colors
       INPUT
-        <ul class="livingstyleguide--color-swatches">
+        <ul class="livingstyleguide--color-swatches -lsg-2-columns">
           <li class="livingstyleguide--color-swatch $red">$red</li>
           <li class="livingstyleguide--color-swatch $blue">$blue</li>
         </ul>
@@ -18,17 +18,32 @@ class ColorsTest < ExampleTestCase
   end
 
   def test_defined_colors
-    LivingStyleGuide::VariablesImporter.stub :variables, %w(red blue) do
-      assert_render_equals <<-INPUT, <<-OUTPUT
-        @colors
-        $orange $green
-      INPUT
-        <ul class="livingstyleguide--color-swatches">
-          <li class="livingstyleguide--color-swatch $orange">$orange</li>
-          <li class="livingstyleguide--color-swatch $green">$green</li>
-        </ul>
-      OUTPUT
-    end
+    assert_render_equals <<-INPUT, <<-OUTPUT
+      @colors
+      $orange $green
+    INPUT
+      <ul class="livingstyleguide--color-swatches -lsg-2-columns">
+        <li class="livingstyleguide--color-swatch $orange">$orange</li>
+        <li class="livingstyleguide--color-swatch $green">$green</li>
+      </ul>
+    OUTPUT
+  end
+
+  def test_rows
+    assert_render_equals <<-INPUT, <<-OUTPUT
+      @colors
+      $pink $purple $gray
+      $turquoise $cyan $black
+    INPUT
+      <ul class="livingstyleguide--color-swatches -lsg-3-columns">
+        <li class="livingstyleguide--color-swatch $pink">$pink</li>
+        <li class="livingstyleguide--color-swatch $purple">$purple</li>
+        <li class="livingstyleguide--color-swatch $gray">$gray</li>
+        <li class="livingstyleguide--color-swatch $turquoise">$turquoise</li>
+        <li class="livingstyleguide--color-swatch $cyan">$cyan</li>
+        <li class="livingstyleguide--color-swatch $black">$black</li>
+      </ul>
+    OUTPUT
   end
 
 end
