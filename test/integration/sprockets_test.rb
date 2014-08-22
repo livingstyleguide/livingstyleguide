94a877ecc00b007c5671e306f93b915a38e3a25c
Nico Hagenburger <nico@hagenburger.net>
2014-08-22 11:45:12 +0200

    refactored file lists to use sass trees

--- test/integration/sprockets_test.rb
+++ test/integration/sprockets_test.rb
@@ -6,8 +6,8 @@ describe "Sprockets integration" do
   describe "sprockets should know when to invalidate cache" do
     template = Tilt.new('test/fixtures/standalone/styleguide.html.lsg')
     context = Minitest::Mock.new
-    %w(style.scss modules/_buttons.scss modules/_buttons.md styleguide.html.lsg).each do |file|
-      context.expect :depend_on, nil, ["test/fixtures/standalone/#{file}"]
+    %w(style.scss modules/_buttons.scss modules/_buttons.md).each do |file|
+      context.expect :depend_on, nil, [file]
     end
 
     template.render context
