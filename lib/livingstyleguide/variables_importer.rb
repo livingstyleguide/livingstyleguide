b710d29946d189153c139110b3192cad75260c23
Nico Hagenburger <nico@hagenburger.net>
2014-08-23 19:51:22 +0200

    use sass to generate variables list

--- lib/livingstyleguide/variables_importer.rb
+++ lib/livingstyleguide/variables_importer.rb
@@ -4,40 +4,23 @@ require 'compass'
 
 module LivingStyleGuide
   class VariablesImporter < Sass::Importers::Base
-    VALID_FILE_NAME = /\A#{Sass::SCSS::RX::IDENT}\Z/
     VARIABLE_IMPORTER_STRING = 'LivingStyleGuide::VariablesImporter'
-    VALID_EXTENSIONS = %w(*.sass *.scss)
-
     TEMPLATE_FOLDER = File.join(File.expand_path('../', __FILE__), 'variables_importer')
     CONTENT_TEMPLATE_FILE = File.join(TEMPLATE_FOLDER, 'content.erb')
     CONTENT_TEMPLATE = ERB.new(File.read(CONTENT_TEMPLATE_FILE))
 
     def find(uri, options)
       if uri =~ /^#{VARIABLE_IMPORTER_STRING}$/
-        @options = options
-        return self.class.sass_engine(uri, all_variables, self, options)
+        variables = options[:living_style_guide].variables
+        binder = LivingStyleGuide::Binding.new(:variables => variables)
+        scss = CONTENT_TEMPLATE.result(binder.get_binding)
+        return ::Sass::Engine.new(scss, syntax: :scss)
       end
       nil
     end
 
     def find_relative(uri, base, options)
       nil
-    end
-
-    def to_s
-      self.class.name
-    end
-
-    def hash
-      self.class.name.hash
-    end
-
-    def eql?(other)
-      other.class == self.class
-    end
-
-    def key(uri, options={})
-      [self.class.name + ":variables:" + File.dirname(File.expand_path(uri)), File.basename(uri)]
     end
 
     def self.variables(uri)
@@ -59,33 +42,6 @@ module LivingStyleGuide
       variables
     end
 
-    def all_variables
-      variables = []
-      test = /^#{File.expand_path(@options[:living_style_guide].options[:root])}/
-      @options[:living_style_guide].files.each do |file|
-        if File.expand_path(file) =~ test
-          sass = File.read(file)
-          variables << sass.scan(%r(\$([a-z\-_]+)\s*:))
-        end
-      end
-      variables.flatten!
-      variables.uniq!
-      variables
-    end
-
-    def self.sass_options(uri, importer, options)
-      options.merge! :filename => uri.gsub(%r{\*/},"*\\/"), :syntax => :scss, :importer => importer
-    end
-
-    def self.sass_engine(uri, name, importer, options)
-      content = content_for_images(uri, name, options[:skip_overrides])
-      Sass::Engine.new(content, sass_options(uri, importer, options))
-    end
-
-    def self.content_for_images(uri, variables, skip_overrides = false)
-      binder = LivingStyleGuide::Binding.new(:variables => variables)
-      CONTENT_TEMPLATE.result(binder.get_binding)
-    end
   end
 
   class Binding < OpenStruct
@@ -94,3 +50,4 @@ module LivingStyleGuide
     end
   end
 end
+
