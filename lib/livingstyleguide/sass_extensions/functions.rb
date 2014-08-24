1fca795219fbe348428f26179fe4a094420834fc
Nico Hagenburger <nico@hagenburger.net>
2014-08-24 04:19:17 +0200

    depricated list-variables; removed variables_importer

--- lib/livingstyleguide/sass_extensions/functions.rb
+++ lib/livingstyleguide/sass_extensions/functions.rb
@@ -1,8 +1,9 @@
 module LivingStyleGuide::SassExtensions::Functions
 
   def list_variables(uri)
+    Sass::Util.sass_warn '`list-variables()` is depricated and will be removed in v2.0.0.'
     uri = uri.value
-    variables = LivingStyleGuide::VariablesImporter.variables(uri)
+    variables = parse_variables(uri)
     variables.map! do |name|
       Sass::Script::String.new(name)
     end
@@ -13,6 +14,26 @@ module LivingStyleGuide::SassExtensions::Functions
     def asset_url(path, prefix)
       Sass::Script::String.new(options[:sprockets][:context].asset_url(path.value, prefix.value))
     end
+  end
+
+  private
+  def parse_variables(uri)
+    uri = uri.dup
+    uri += '.s?ss' unless uri =~ /\.s[ac]ss$/
+    uri.gsub! %r{^(.*)/(.+)$}, '\1/{_,}\2'
+    variables = []
+    paths = [Compass.configuration.sass_path, Compass.configuration.additional_import_paths].flatten
+    paths.each do |path|
+      if path.is_a? String
+        Dir.glob(File.join(path, uri)).each do |file|
+          sass = File.read(file)
+          variables << sass.scan(%r(\$([a-z0-9\-_]+)\s*:))
+        end
+      end
+    end
+    variables.flatten!
+    variables.uniq!
+    variables
   end
 
 end
