26a6b4e313ec4daa2ada9877e24c49813ec3e951
Nico Hagenburger <nico@hagenburger.net>
2014-08-24 03:39:20 +0200

    refactored variables importer

--- lib/livingstyleguide/integration/compass.rb
+++ lib/livingstyleguide/integration/compass.rb
@@ -1,5 +1,3 @@
-Compass.configuration.add_import_path LivingStyleGuide::VariablesImporter.new
-
 base_directory = File.join(File.dirname(__FILE__), '..', '..', '..')
 Compass::Frameworks.register 'livingstyleguide', path: base_directory
 
