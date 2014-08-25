94bc24d7762166b8bd488c296afb33d0004f0844
Nico Hagenburger <nico@hagenburger.net>
2014-08-25 10:11:10 +0200

    allow *.lsg and *.html.lsg files as source

--- test/integration/command_line_test.rb
+++ test/integration/command_line_test.rb
@@ -2,10 +2,16 @@ require 'test_helper'
 
 describe "LivingStyleGuide::CommandLineInterface" do
 
-  it "should output the style guide" do
-    `./bin/livingstyleguide compile test/fixtures/standalone/style.html.lsg`
-    File.exists?('test/fixtures/standalone/style.html').must_equal true
-    File.delete 'test/fixtures/standalone/style.html'
+  it "should output the style guide from *.html.lsg source" do
+    `./bin/livingstyleguide compile test/fixtures/standalone/styleguide.html.lsg`
+    File.exists?('test/fixtures/standalone/styleguide.html').must_equal true
+    File.delete 'test/fixtures/standalone/styleguide.html'
+  end
+
+  it "should output the style guide from *.lsg source" do
+    `./bin/livingstyleguide compile test/fixtures/standalone/styleguide.lsg`
+    File.exists?('test/fixtures/standalone/styleguide.html').must_equal true
+    File.delete 'test/fixtures/standalone/styleguide.html'
   end
 
 end
