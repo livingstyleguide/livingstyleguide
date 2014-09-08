028feff7b6bb50d6a3f154314fbdf00af5735d82
Nico Hagenburger <nico@hagenburger.net>
2014-09-08 23:35:25 +0200

    cli: added output file to compile

--- lib/livingstyleguide/command_line_interface.rb
+++ lib/livingstyleguide/command_line_interface.rb
@@ -9,16 +9,16 @@ require 'tilt'
 module LivingStyleGuide
   class CommandLineInterface < Thor
 
-    desc 'compile filename', 'Compiles the living style guide to HTML.'
-    def compile(file)
-      output_file = file.sub(/(\.html)?\.lsg$/, '.html')
+    desc 'compile input output', 'Compiles the living style guide to HTML.'
+    def compile(input, output = nil)
+      output = input.sub(/(\.html)?\.lsg$/, '.html') if output.nil?
       if defined?(Compass)
-        Compass.add_project_configuration 
-        output_file.sub! /^#{Compass.configuration.sass_dir}/, Compass.configuration.css_dir
+        Compass.add_project_configuration
+        output = output.sub /^#{Compass.configuration.sass_dir}/, Compass.configuration.css_dir
       end
-      html = Tilt.new(file).render
-      File.write output_file, html
-      puts "Successfully generated a living style guide at #{output_file}."
+      html = Tilt.new(input).render
+      File.write output, html
+      puts "Successfully generated a living style guide at #{output}."
     end
 
   end
