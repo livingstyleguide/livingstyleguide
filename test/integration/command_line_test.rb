cc566bac8f1dd571ebef8b0c3db6c244ec3fa402
Nico Hagenburger <nico@hagenburger.net>
2014-09-09 12:23:22 +0200

    simpler paths ins tests

--- test/integration/command_line_test.rb
+++ test/integration/command_line_test.rb
@@ -4,41 +4,44 @@ require 'open3'
 describe "LivingStyleGuide::CommandLineInterface" do
 
   it "should output the style guide from *.html.lsg source" do
-    cli('compile test/fixtures/standalone/styleguide.html.lsg') do
-      File.exists?('test/fixtures/standalone/styleguide.html').must_equal true
+    cli('compile styleguide.html.lsg') do
+      File.exists?('styleguide.html').must_equal true
     end
   end
 
   it "should output the style guide from *.lsg source" do
-    cli('compile test/fixtures/standalone/styleguide.lsg') do
-      File.exists?('test/fixtures/standalone/styleguide.html').must_equal true
+    cli('compile styleguide.lsg') do
+      File.exists?('styleguide.html').must_equal true
     end
   end
 
   it "should use different output file" do
-    cli('compile test/fixtures/standalone/styleguide.lsg test/fixtures/standalone/hello-world.html') do
-      File.exists?('test/fixtures/standalone/hello-world.html').must_equal true
+    cli('compile styleguide.lsg hello-world.html') do
+      File.exists?('hello-world.html').must_equal true
     end
   end
 
   it "should read from STDIN and write to STDOUT" do
-    cli('compile', 'source: test/fixtures/standalone/style.scss') do
-      File.exists?('test/fixtures/standalone/styleguide.html').must_equal false
+    cli('compile', 'source: style.scss') do
+      File.exists?('styleguide.html').must_equal false
     end.must_match %r(<button class="button">)
   end
 
   def cli(command, input = nil, &block)
-    files = Dir.glob('test/fixtures/standalone/*')
-    stdin, stdout = Open3.popen2("./bin/livingstyleguide #{command}")
+    current_path = Dir.pwd
+    Dir.chdir 'test/fixtures/standalone'
+    files = Dir.glob('*')
+    stdin, stdout = Open3.popen2("../../../bin/livingstyleguide #{command}")
     stdin.puts input
     stdin.close
     result = stdout.read
 
     yield
 
-    (Dir.glob('test/fixtures/standalone/*') - files).each do |file|
+    (Dir.glob('*') - files).each do |file|
       File.unlink file
     end
+    Dir.chdir current_path
     result
   end
 
