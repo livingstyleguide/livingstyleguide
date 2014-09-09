ea8c4cde13b5446d6231b5563beccc279a75fba8
Nico Hagenburger <nico@hagenburger.net>
2014-09-09 12:27:59 +0200

    check cli result within block

--- test/integration/command_line_test.rb
+++ test/integration/command_line_test.rb
@@ -4,27 +4,30 @@ require 'open3'
 describe "LivingStyleGuide::CommandLineInterface" do
 
   it "should output the style guide from *.html.lsg source" do
-    cli('compile styleguide.html.lsg') do
+    cli 'compile styleguide.html.lsg' do
       File.exists?('styleguide.html').must_equal true
     end
   end
 
   it "should output the style guide from *.lsg source" do
-    cli('compile styleguide.lsg') do
+    cli 'compile styleguide.lsg' do
       File.exists?('styleguide.html').must_equal true
     end
   end
 
   it "should use different output file" do
-    cli('compile styleguide.lsg hello-world.html') do
+    cli 'compile styleguide.lsg hello-world.html' do
       File.exists?('hello-world.html').must_equal true
     end
   end
 
   it "should read from STDIN and write to STDOUT" do
-    cli('compile', 'source: style.scss') do
+    cli 'compile', <<-STDIN do |stdout|
+      source: style.scss
+    STDIN
       File.exists?('styleguide.html').must_equal false
-    end.must_match %r(<button class="button">)
+      stdout.must_match %r(<button class="button">)
+    end
   end
 
   def cli(command, input = nil, &block)
@@ -34,15 +37,13 @@ describe "LivingStyleGuide::CommandLineInterface" do
     stdin, stdout = Open3.popen2("../../../bin/livingstyleguide #{command}")
     stdin.puts input
     stdin.close
-    result = stdout.read
 
-    yield
+    yield stdout.read
 
     (Dir.glob('*') - files).each do |file|
       File.unlink file
     end
     Dir.chdir current_path
-    result
   end
 
 end
