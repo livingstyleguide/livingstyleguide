43d24d89c0403cee846c0273814b1131aa4998da
Frederik Fix <ich@derfred.com>
2015-01-14 19:27:34 +0100

    fix breakage due to change in sass-rails

--- lib/livingstyleguide/tilt_template.rb
+++ lib/livingstyleguide/tilt_template.rb
@@ -40,7 +40,7 @@ module LivingStyleGuide
       options[:syntax]    = @options[:syntax]
       options[:sprockets] = { context: @scope }
       options[:custom]    = { sprockets_context: @scope }
-      if defined?(Sass::Rails)
+      if defined?(Sass::Rails::Resolver)
         options[:custom][:resolver] = Sass::Rails::Resolver.new(@scope)
       end
       options
