376deb21ff5bc7ea8b6c223cf5d0bdbdad32942f
Nico Hagenburger <nico@hagenburger.net>
2015-01-23 21:30:22 +0100

    rewrote font-example with template

--- lib/livingstyleguide/filters/font_example.rb
+++ lib/livingstyleguide/filters/font_example.rb
@@ -19,3 +19,11 @@ LivingStyleGuide::Example.add_filter :font_example do |font|
   end
 end
 
+LivingStyleGuide.add_filter :font_example do |font, text = nil|
+  text ||= LivingStyleGuide::Engine.default_options[:font_example][:text]
+  text = ERB::Util.html_escape(text)
+  text.strip!
+  text.gsub!(/\n/, "<br>\n")
+  font = ERB::Util.html_escape(font)
+  ERB.new(File.read("#{File.dirname(__FILE__)}/../templates/font-example.html.erb")).result(binding)
+end
