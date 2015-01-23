169d0a8b44527b6e9b7c3a772a0c4865c526b4a0
Nico Hagenburger <nico@hagenburger.net>
2015-01-23 20:57:09 +0100

    rewrote add wrapper class

--- lib/livingstyleguide/filters/add_wrapper_class.rb
+++ lib/livingstyleguide/filters/add_wrapper_class.rb
@@ -2,3 +2,6 @@ LivingStyleGuide::Example.add_filter :add_wrapper_class do |css_class|
   add_wrapper_class css_class
 end
 
+LivingStyleGuide.add_filter :add_wrapper_class do |css_class|
+  document.classes << css_class
+end
