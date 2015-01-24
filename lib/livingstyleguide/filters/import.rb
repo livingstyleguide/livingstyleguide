c2d3258ef0a68a6d1f03f12d38b3736100346995
Nico Hagenburger <nico@hagenburger.net>
2015-01-24 13:07:02 +0100

    allow glob imports

--- lib/livingstyleguide/filters/import.rb
+++ lib/livingstyleguide/filters/import.rb
@@ -1,6 +1,8 @@
 require 'tilt'
 
-LivingStyleGuide.add_filter :import do |file|
-  file << '.lsg' unless file =~ /\.(\w+|\*)$/
-  ::Tilt.new(file).render
+LivingStyleGuide.add_filter :import do |glob|
+  glob << '.lsg' unless glob =~ /\.(\w+|\*)$/
+  Dir.glob(glob).map do |file|
+    ::Tilt.new(file).render
+  end.join
 end
