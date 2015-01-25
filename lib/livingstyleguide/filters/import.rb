a99366f748586a6a18ecd86d166b4138e729961a
Nico Hagenburger <nico@hagenburger.net>
2015-01-25 14:33:23 +0100

    allow passing data into imports

--- lib/livingstyleguide/filters/import.rb
+++ lib/livingstyleguide/filters/import.rb
@@ -1,9 +1,12 @@
 require 'tilt'
 
-LivingStyleGuide.add_filter :import do |glob|
+LivingStyleGuide.add_filter :import do |glob, data = nil|
   glob << '.lsg' unless glob =~ /\.(\w+|\*)$/
   glob.gsub!(/[^\/]+$/, '{_,}\\0')
+  if data
+    data = JSON.parse("{#{data}}")
+  end
   Dir.glob(glob).map do |file|
-    ::Tilt.new(file).render
+    ::Tilt.new(file).render(nil, data)
   end.join
 end
