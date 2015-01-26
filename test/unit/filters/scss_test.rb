40c2d07c32d66c805a9b5bb28383790fdcc4622c
Nico Hagenburger <nico@hagenburger.net>
2015-01-26 13:40:26 +0100

    pass scss, head, header, and footer to root document

--- test/unit/filters/scss_test.rb
+++ test/unit/filters/scss_test.rb
@@ -15,6 +15,27 @@ class JavaScriptTest < ExampleTestCase
     assert_match(/##{doc.id} #adding-scss\s*\{\s*background: red;\s*\}/m, doc.css)
   end
 
+  def test_adding_scss_inside_examples
+    doc = LivingStyleGuide::Document.new do <<-INPUT.unindent
+        @scss {
+          #adding-scss {
+            background: red;
+          }
+        }
+        ```
+        @scss {
+          #adding-more-scss {
+            background: green;
+          }
+        }
+        ```
+      INPUT
+    end
+    doc.render
+    assert_match(/##{doc.id} #adding-scss\s*\{\s*background: red;\s*\}/m, doc.css)
+    assert_match(/#section-\w{6} #adding-more-scss\s*\{\s*background: green;\s*\}/m, doc.css)
+  end
+
 end
 
 
