daf99b8185fe62e19ec377667addbfbe9d67d0d1
Nico Hagenburger <nico@hagenburger.net>
2015-02-04 22:17:36 +0100

    upgraded coffee-script filter

--- test/unit/filters/coffee_script_test.rb
+++ test/unit/filters/coffee_script_test.rb
@@ -1,19 +1,29 @@
-require 'example_test_helper'
+require 'document_test_helper'
 
-class CoffeeScriptTest < ExampleTestCase
+class CoffeeScriptTest < DocumentTestCase
 
   def test_coffee_script
     assert_render_equals <<-INPUT, <<-OUTPUT
       @coffee-script
       alert "Hello world!"
     INPUT
-      <div class="livingstyleguide--example -lsg-for-javascript"> <script>(function() { alert("Hello world!"); }).call(this); </script> </div>
+      <section class="livingstyleguide--example livingstyleguide--coffee-example" id="section-9de304"> <script>(function() { alert(\"Hello world!\"); }).call(this); </script>
       <pre class="livingstyleguide--code-block">
-        <code class="livingstyleguide--code">alert "Hello world!"</code>
-      </pre>
+        <code class="livingstyleguide--code">alert "Hello world!" </code>
+      </pre> </section>
+    OUTPUT
+  end
+
+  def test_coffee
+    assert_render_equals <<-INPUT, <<-OUTPUT
+      @coffee
+      alert "Hello world!"
+    INPUT
+      <section class="livingstyleguide--example livingstyleguide--coffee-example" id="section-1791e9"> <script>(function() { alert(\"Hello world!\"); }).call(this); </script>
+      <pre class="livingstyleguide--code-block">
+        <code class="livingstyleguide--code">alert "Hello world!" </code>
+      </pre> </section>
     OUTPUT
   end
 
 end
-
-
