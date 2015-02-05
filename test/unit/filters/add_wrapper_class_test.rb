58a2657a1710aec19fcf859243c63bd5dffc7384
Nico Hagenburger <nico@hagenburger.net>
2015-02-05 01:32:26 +0100

    fixed test

--- test/unit/filters/add_wrapper_class_test.rb
+++ test/unit/filters/add_wrapper_class_test.rb
@@ -8,7 +8,7 @@ class AddWrapperClassTest < DocumentTestCase
       @add-wrapper-class my-class
       ```
     INPUT
-      class="livingstyleguide--example.+?my-class">
+      class="livingstyleguide--example.+?my-class"
     OUTPUT
   end
 
