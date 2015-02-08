968943745a643fb58e98b3e1a614d2c698c7c679
Nico Hagenburger <nico@hagenburger.net>
2015-02-08 20:04:05 +0100

    sprockets takes care of this automatically now, no action needed

--- test/integration/sprockets_test.rb
+++ test/integration/sprockets_test.rb
@@ -6,7 +6,6 @@ describe "Sprockets integration" do
   describe "sprockets should know when to invalidate cache" do
     template = Tilt.new('test/fixtures/standalone/styleguide.html.lsg')
     context = Minitest::Mock.new
-    # TODO: modules/_buttons.scss
     %w(style.scss modules/_buttons.lsg).each do |file|
       file = File.expand_path("test/fixtures/standalone/#{file}")
       context.expect :depend_on, nil, [file]
