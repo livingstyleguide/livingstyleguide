8aa2cb0822d7c5cbb12c96017f74a0b844681208
Nico Hagenburger <nico@hagenburger.net>
2015-02-08 19:16:33 +0100

    refactored sass compiling; fixes #98, fixes #103, fixed #105

--- lib/livingstyleguide/integration/rails.rb
+++ lib/livingstyleguide/integration/rails.rb
@@ -4,6 +4,7 @@ if defined?(Rails) and defined?(Rails::Railtie)
   class LivingStyleGuideRailtie < Rails::Railtie
     initializer 'living_style_guide.assets' do
       Rails.application.assets.register_engine('.lsg', ::LivingStyleGuide::Document)
+      LivingStyleGuide.default_options[:scss_template] = ::Sass::Rails::ScssTemplate
     end
   end
 
