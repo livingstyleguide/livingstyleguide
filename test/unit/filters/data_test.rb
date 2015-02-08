a9ff6459ccfc7653c82dcc841dbd1d09141c3ce0
Nico Hagenburger <nico@hagenburger.net>
2015-02-08 21:04:07 +0100

    fixed parsing of erb templates

--- test/unit/filters/data_test.rb
+++ test/unit/filters/data_test.rb
@@ -2,12 +2,24 @@ require 'document_test_helper'
 
 class DataTest < DocumentTestCase
 
-  def test_data
-    assert_render_match <<-INPUT, <<-OUTPUT, type: 'erb'
+  def test_data_for_erb
+    assert_render_match <<-INPUT, <<-OUTPUT, type: :erb
       @data {
-        "foo": "Bar"
+        "data_foo": "Bar"
       }
-      <div><%= foo %></div>
+      <div><%= data_foo %></div>
+    INPUT
+      <div>Bar</div>
+    OUTPUT
+  end
+
+  def test_data_for_haml
+    assert_render_match <<-INPUT, <<-OUTPUT
+      @data {
+        "data_foo": "Bar"
+      }
+      @haml
+      %div= data_foo
     INPUT
       <div>Bar</div>
     OUTPUT
