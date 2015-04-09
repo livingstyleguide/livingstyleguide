2962d118150ec4c08232a019faf11621cc58e4e6
Nico Hagenburger <nico@hagenburger.net>
2015-04-09 23:12:23 +0200

    add type command to allow any templating language supported by tilt

---
+++ test/unit/filters/type_test.rb
@@ -0,0 +1,37 @@
+require 'document_test_helper'
+
+class TypeTest < DocumentTestCase
+
+  def test_erb
+    assert_render_match <<-INPUT, <<-OUTPUT
+      @type erb
+      <div>
+        <div class="lorem"><%= "Ipsum" %></div>
+      </div>
+    INPUT
+      <div> <div class="lorem">Ipsum</div> </div>
+    OUTPUT
+  end
+
+  def test_haml
+    assert_render_match <<-INPUT, <<-OUTPUT
+      @type haml
+      %div
+        .lorem Ipsum
+    INPUT
+      <div> <div class='lorem'>Ipsum</div> </div>
+    OUTPUT
+  end
+
+  def test_markdown
+    assert_render_match <<-INPUT, <<-OUTPUT
+      @type markdown
+      # Ipsum
+      Lorem
+    INPUT
+      <h1>Ipsum</h1>
+      <p>Lorem</p>
+    OUTPUT
+  end
+
+end
