2abf7f81b08bd5a5e0c397cc155ad9de82b57ce2
Nico Hagenburger <nico@hagenburger.net>
2015-04-09 22:09:08 +0200

    differ markdown from lsg rendering

--- test/document_test_helper.rb
+++ test/document_test_helper.rb
@@ -8,7 +8,7 @@ class DocumentTestCase < Minitest::Test
 
   def assert_render_equals(input, expected_output, options = {})
     @doc = @class.new(options[:file]){ input.unindent }
-    @doc.type = options[:type] || :markdown
+    @doc.type = options[:type] || :lsg
     @doc.template = options[:template] || 'plain'
     output = @doc.render
     assert_equal(normalize(expected_output), normalize(output))
@@ -16,7 +16,7 @@ class DocumentTestCase < Minitest::Test
 
   def assert_render_match(input, expected_output, options = {})
     @doc = @class.new(options[:file]){ input.gsub(/\n\n/, "\n      \n").unindent }
-    @doc.type = options[:type] || :markdown
+    @doc.type = options[:type] || :lsg
     @doc.template = options[:template] || 'plain'
     output = @doc.render
     assert_match(/#{normalize(expected_output)}/, normalize(output))
