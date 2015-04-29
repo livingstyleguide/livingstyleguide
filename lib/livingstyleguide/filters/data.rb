eaa93c1d451a5046747a66267c87b37a119b5a74
Nico Hagenburger <nico@hagenburger.net>
2015-04-29 23:58:47 +0200

    added yaml support for data command

--- lib/livingstyleguide/filters/data.rb
+++ lib/livingstyleguide/filters/data.rb
@@ -1,6 +1,11 @@
 require 'json'
+require 'yaml'
 
 LivingStyleGuide.add_filter :data do |arguments, options, data|
-  document.locals.merge! JSON.parse("{#{data}}")
+  if options[:format] == "yaml"
+    document.locals.merge! Psych.load("{#{data}}")
+  else
+    document.locals.merge! JSON.parse("{#{data}}")
+  end
   nil
 end
