24d803bc7405babace2b0ab1da211006d6001801
Nico Hagenburger <nico@hagenburger.net>
2015-05-01 10:27:11 +0200

    style filters should not output the options hash

--- lib/livingstyleguide/filters/style.rb
+++ lib/livingstyleguide/filters/style.rb
@@ -2,6 +2,6 @@ LivingStyleGuide.add_filter :style do |arguments, options, block|
   options.delete :block_type
   options.each do |key, value|
     document.scss << %Q($livingstyleguide--#{key.to_s.gsub("_", "-")}: #{value};\n)
-    nil
   end
+  nil
 end
