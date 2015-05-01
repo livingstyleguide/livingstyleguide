3fe99e2571a7a04c2c6d4641eb12cfb2259cc5a2
Nico Hagenburger <nico@hagenburger.net>
2015-05-01 23:41:16 +0200

    fixed default command

--- lib/livingstyleguide/filters/default.rb
+++ lib/livingstyleguide/filters/default.rb
@@ -1,4 +1,6 @@
 LivingStyleGuide.add_filter :default do |arguments, options, block|
-  document.defaults[arguments.first.to_sym] ||= {}
-  document.defaults[arguments.first.to_sym].merge(options)
+  key = arguments.first.gsub("-", "_").to_sym
+  document.defaults[key] ||= {}
+  document.defaults[key].merge!(options)
+  nil
 end
