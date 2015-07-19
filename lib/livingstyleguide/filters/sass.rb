78ccb8031d3dd2941c9b5f6764e33feceabd27c4
Nico Hagenburger <nico@hagenburger.net>
2015-07-19 17:45:10 +0200

    split up css command into css, sass and scss; closes #146

---
+++ lib/livingstyleguide/filters/sass.rb
@@ -0,0 +1,28 @@
+LivingStyleGuide.add_filter :scss do |arguments, options, scss|
+  file = arguments.first
+  if file
+    if document.file
+      file = File.join(File.dirname(document.file), file)
+    end
+    document.depend_on file
+    document.scss << %Q(@import "#{file}";\n)
+  else
+    document.scss << "##{document.id.gsub(/[\/\.]/, '\\\0')} {\n#{scss}\n}\n"
+  end
+  nil
+end
+
+LivingStyleGuide.add_filter :sass do |arguments, options, sass|
+  file = arguments.first
+  if file
+    if document.file
+      file = File.join(File.dirname(document.file), file)
+    end
+    document.depend_on file
+    document.scss << %Q(@import "#{file}";\n)
+  else
+    scss = Sass::Engine.new(sass).to_tree.to_scss
+    document.scss << "##{document.id.gsub(/[\/\.]/, '\\\0')} {\n#{scss}\n}\n"
+  end
+  nil
+end
