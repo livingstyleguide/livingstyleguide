f8f18a293de6d8fd24f7d7fa31514b8323a72b48
fanxhe-pair <fanxhe.team@gmail.com>
2015-07-24 20:36:43 -0500

    Add button to copy html, javascript, haml and coffeescript inside code blocks

--- lib/livingstyleguide/filters/javascript.rb
+++ lib/livingstyleguide/filters/javascript.rb
@@ -1,5 +1,7 @@
 LivingStyleGuide.add_filter :javascript do |arguments, options, block|
   document.type = :javascript
   document.template = :javascript
+  document.head << ERB.new(File.read("#{File.dirname(__FILE__)}/../templates/javascript-copy.html.erb")).result
+  document.head << ERB.new(File.read("#{File.dirname(__FILE__)}/../templates/code-example.html.erb")).result
   nil
 end
