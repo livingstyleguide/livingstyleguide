446e4a30c3da82731d560012f49b30ff35a3f40f
fanxhe-pair <fanxhe.team@gmail.com>
2015-07-31 12:29:04 -0500

    Fix problem with duplicated document script tags

--- lib/livingstyleguide/filters/coffee_script.rb
+++ lib/livingstyleguide/filters/coffee_script.rb
@@ -1,7 +1,5 @@
 LivingStyleGuide.add_filter :coffee_script, :coffee do |arguments, options, block|
   document.type = :coffee
   document.template = :javascript
-  document.head << ERB.new(File.read("#{File.dirname(__FILE__)}/../templates/javascript-copy.html.erb")).result
-  document.head << ERB.new(File.read("#{File.dirname(__FILE__)}/../templates/code-example.html.erb")).result
   nil
 end
