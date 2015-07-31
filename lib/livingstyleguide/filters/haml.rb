c88b975adce3dea702ba3537a90dc117621cf097
fanxhe-pair <fanxhe.team@gmail.com>
2015-07-31 12:29:04 -0500

    Fix problem with duplicated document script tags

--- lib/livingstyleguide/filters/haml.rb
+++ lib/livingstyleguide/filters/haml.rb
@@ -1,6 +1,4 @@
 LivingStyleGuide.add_filter :haml do |arguments, options, block|
   document.type = :haml
-  document.head << ERB.new(File.read("#{File.dirname(__FILE__)}/../templates/javascript-copy.html.erb")).result
-  document.head << ERB.new(File.read("#{File.dirname(__FILE__)}/../templates/code-example.html.erb")).result
   nil
 end
