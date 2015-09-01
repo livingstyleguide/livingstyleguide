61450b5e7433da5e406c04e78d7b4cc7962684e4
fanxhe-pair <fanxhe.team@gmail.com>
2015-09-01 16:36:22 -0500

    Feedback changes:
    - Use localstorage instead of cookies
    - Use only one variable for toggle-code and its button
    - Change filter name to toggle-code
    - Refactor classes names in css and javascript to show code by default
    - Add var prefix to variables
    - Add lsg prefix to css classes

--- lib/livingstyleguide/filters/show_hide.rb
+++ lib/livingstyleguide/filters/show_hide.rb
@@ -1,4 +1,4 @@
-LivingStyleGuide.add_filter :show_hide do |arguments, options, content|
-  document.header << ERB.new(File.read("#{File.dirname(__FILE__)}/../templates/show_hide.html.erb")).result(binding)
+LivingStyleGuide.add_filter :toggle_code do |arguments, options, content|
+  document.header << ERB.new(File.read("#{File.dirname(__FILE__)}/../templates/toggle_code.html.erb")).result(binding)
   nil
 end
