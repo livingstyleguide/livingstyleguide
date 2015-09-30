845eaf6cd0a89f4b0cbdd79a305f9892c396d4e1
fanxhe-pair <fanxhe.team@gmail.com>
2015-09-30 18:32:20 -0500

    Add tests for before and after filters

---
+++ test/unit/filters/before_test.rb
@@ -0,0 +1,14 @@
+require 'document_test_helper'
+
+class BeforeTest < DocumentTestCase
+
+  def test_before
+    assert_render_match <<-INPUT, <<-OUTPUT, template: :layout
+      @before:
+        <h1>This my before</h1>
+    INPUT
+      <section class="lsg--before">.*<h1>This my before</h1>.*</section>
+    OUTPUT
+  end
+
+end
