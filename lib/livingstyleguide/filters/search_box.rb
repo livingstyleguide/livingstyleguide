c1eb053f044aad942bba73eff950cef3e79ff5a2
fanxhe-pair <fanxhe.team@gmail.com>
2015-09-30 14:47:31 -0500

    Rename template file name inside search-box filter

--- lib/livingstyleguide/filters/search_box.rb
+++ lib/livingstyleguide/filters/search_box.rb
@@ -5,6 +5,6 @@ LivingStyleGuide.default_options[:search_box] = {
 LivingStyleGuide.add_filter :search_box do |arguments, options, content|
   placeholder = options[:placeholder] || LivingStyleGuide.default_options[:search_box][:placeholder]
   document.header << ERB.new(File.read("#{File.dirname(__FILE__)}/../templates/search-box.html.erb")).result(binding)
-  document.javascript << ERB.new(File.read("#{File.dirname(__FILE__)}/../templates/scripts/search.html.erb")).result(binding)
+  document.javascript << ERB.new(File.read("#{File.dirname(__FILE__)}/../templates/scripts/search.js.erb")).result(binding)
   nil
 end
