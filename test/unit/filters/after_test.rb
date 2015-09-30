35474f69cc297a5186c8db822f58f6725dc846f8
fanxhe-pair <fanxhe.team@gmail.com>
2015-09-30 18:32:20 -0500

    Add tests for before and after filters

---
+++ test/unit/filters/after_test.rb
@@ -0,0 +1,14 @@
+require 'document_test_helper'
+
+class AfterTest < DocumentTestCase
+
+  def test_after
+    assert_render_match <<-INPUT, <<-OUTPUT, template: :layout
+      @after:
+        <h1>This my after</h1>
+    INPUT
+      <section class="lsg--after">.*<h1>This my after</h1>.*</section>
+    OUTPUT
+  end
+
+end
