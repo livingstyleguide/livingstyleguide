24f56059452c0e7790db15f9515eed4d524bc80e
fanxhe-pair <fanxhe.team@gmail.com>
2015-10-05 19:43:36 -0500

    Changes in the toggle-code command:
    - Include in the document's before section
    - Remove the container
    - Remove the container positioning scss code
    - Rename template file to match other templates
    - Add tests
    
     Fix feedback by hound ci

--- lib/livingstyleguide/commands/toggle_code.rb
+++ lib/livingstyleguide/commands/toggle_code.rb
@@ -1,4 +1,4 @@
 LivingStyleGuide.command :toggle_code do |arguments, options, content|
-  document.header << LivingStyleGuide.template("toggle_code.html.erb", binding)
+  document.before << LivingStyleGuide.template("toggle-code.html.erb", binding)
   nil
 end
