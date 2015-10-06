299ed6880c6efd6216ef8eb43286fdc2d932d85d
fanxhe-pair <fanxhe.team@gmail.com>
2015-10-05 21:50:04 -0500

    Fix feedback by hound ci

--- test/unit/commands/toggle_code_test.rb
+++ test/unit/commands/toggle_code_test.rb
@@ -1,7 +1,6 @@
 require "document_test_helper"
 
 class ToggleCode < DocumentTestCase
-
   def test_toggle_code
     assert_render_equals <<-INPUT, <<-OUTPUT, template: :"toggle-code"
       @toggle-code
@@ -14,8 +13,9 @@ class ToggleCode < DocumentTestCase
     assert_render_match <<-INPUT, <<-OUTPUT, template: :layout
       @toggle-code
     INPUT
-      .*<section class="lsg--before">.*<input type="button" class="lsg--code-switch">.*</section>.*
+      .*<section class="lsg--before">
+          .*<input type="button" class="lsg--code-switch">.*
+      .*</section>.*
     OUTPUT
   end
-
 end
