7c9718fdc1c0c2793406bda19170aefcb782d901
Nico Hagenburger <nico@hagenburger.net>
2015-11-06 14:20:41 +0100

    adds load path of the current file for require
    Require Ruby files relative to the current LSG file.

--- lib/livingstyleguide/commands/require.rb
+++ lib/livingstyleguide/commands/require.rb
@@ -1,4 +1,6 @@
 LivingStyleGuide.command :require do |arguments, options, block|
+  current_path = File.expand_path(document.path)
+  $LOAD_PATH << current_path unless $LOAD_PATH.include?(current_path)
   Kernel.require arguments.first
   nil
 end
