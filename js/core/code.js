f592f50c76debe79d1b4c954db8a65f42ce8949e
Nico Hagenburger <nico@hagenburger.net>
2018-02-04 22:22:28 +0100

    Allow custom IDs and CSS classes in code blocks

--- js/core/code.js
+++ js/core/code.js
@@ -1,13 +1,16 @@
+const Config = require('./config')
+
 module.exports = class Code {
   constructor (source, infoString, config) {
     this.source = source
     this.parts = ['code']
-    this.config = config
-    this._parseLang(infoString)
+    this.config = config || new Config()
+    this.classList = []
+    this._parseInfoString(infoString)
   }
 
   render () {
-    let html = `<div>`
+    let html = `<div${this._renderAttributes()}>`
     this.parts.forEach((part) => {
       html += (this.config.renderers[part] || (() => `Missing renderer \`${part}\`.`))(this)
     })
@@ -16,7 +19,6 @@ module.exports = class Code {
   }
 
   _parseLang (infoString) {
-    if (!infoString) return
     const match = infoString.match(/^(.+?\.)?(.+)$/)
     if (match) {
       if (match[1]) this.filename = infoString
@@ -24,5 +26,31 @@ module.exports = class Code {
     } else {
       this.lang = infoString
     }
+  }
+
+  _parseInfoString (infoString) {
+    if (!infoString) return
+    const options = infoString.split(/ +/)
+    if (options[0].match(/^[^#.]/)) {
+      this._parseLang(options.shift())
+    }
+    options.forEach((option) => {
+      if (option.match(/^\.(.+)$/)) {
+        this.classList.push(option.slice(1))
+      } else if (option.match(/^#(.+)$/)) {
+        this.id = option.slice(1)
+      }
+    })
+  }
+
+  _renderAttributes () {
+    let attributes = ''
+    if (this.classList.length > 0) {
+      attributes += ` class="${this.classList.sort().join(' ')}"`
+    }
+    if (this.id) {
+      attributes += ` id="${this.id}"`
+    }
+    return attributes
   }
 }
--- js/core/test/code.test.js
+++ js/core/test/code.test.js
@@ -23,3 +23,28 @@ describe('parsing of filename and language', () => {
     expect(code.filename).to.equal('_my-file.sass')
   })
 })
+
+describe('custom ID and classes', () => {
+  it('should use a custom ID', () => {
+    const code1 = new Code('', '#my-id')
+    expect(code1.render()).to.match(/id="my-id"/)
+    const code2 = new Code('', 'html #my-id')
+    expect(code2.render()).to.match(/id="my-id"/)
+  })
+
+  it('should add a custom class', () => {
+    const code1 = new Code('', '.my-class')
+    expect(code1.render()).to.match(/class="[^"]*my-class[^"]*"/)
+    const code2 = new Code('', '.my-class .another-class')
+    expect(code2.render()).to.match(/class="[^"]*another-class[^"]+my-class[^"]*"/)
+    const code3 = new Code('', 'html .my-class')
+    expect(code3.render()).to.match(/class="[^"]*my-class[^"]*"/)
+    const code4 = new Code('', 'html .my-class .another-class')
+    expect(code4.render()).to.match(/class="[^"]*another-class[^"]+my-class[^"]*"/)
+  })
+
+  it('should sort custom classes alphabetically just for the beauty of it', () => {
+    const code1 = new Code('', '.b .a .f .c .e .d')
+    expect(code1.render()).to.match(/class="[^"]*a[^"]+b[^"]+c[^"]+d[^"]+e[^"]+f[^"]*"/)
+  })
+})
